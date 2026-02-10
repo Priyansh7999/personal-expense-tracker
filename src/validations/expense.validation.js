b61ab78ffaaab586351653d533ac69c6c672215b
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-10 22:23:00 +0530

    feat : add validation for transaction date fields

--- src/validations/expense.validation.js
+++ src/validations/expense.validation.js
@@ -14,3 +14,15 @@ const paymentMethodValidation = z.string().trim().transform((val) => val.toLower
 });
 const amountValidation = z.number({required_error: 'Amount is required',invalid_type_error: 'Amount must be a number',}).positive('Amount must be greater than 0');
 
+
+const transactionDateValidation = z.string().refine((dateStr) => {
+  const [year, month, day] = dateStr.split('-').map(Number);
+  const date = new Date(year, month - 1, day)
+  if (isNaN(date.getTime())) return false;
+  const now = new Date();
+  const fyStartYear = now.getMonth() >= 3 ? now.getFullYear() : now.getFullYear() - 1;
+  const fyStart = new Date(fyStartYear, 3, 1);
+  const fyEnd = new Date(fyStartYear + 1, 2, 31);
+
+  return date >= fyStart && date <= fyEnd;
+}, { message: 'Transaction date must be within current financial year' });
