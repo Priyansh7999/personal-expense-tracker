b3774ecbe81a71f1e2013c505f35ee3603141e76
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-10 23:58:52 +0530

    feat: add filtering for getAllExpenses by category, payment method,date,amount

--- src/repositories/expense.repository.js
+++ src/repositories/expense.repository.js
@@ -1,7 +1,8 @@
 const db = require('../db/index');
 const { expenses } = require('../models/schema/expenses.schema');
 const { categories } = require('../models/schema/categories.schema');
-const { eq } = require('drizzle-orm');
+const { eq, gte, lte } = require('drizzle-orm');
+
 class ExpenseRepository {
   async getCategoryByName(name){
     const result = await db.select().from(categories).where(eq(categories.name, name));
@@ -12,8 +13,24 @@ class ExpenseRepository {
     return inserted;
   }
 
-  async getAllExpenses(){
-    return await db.select().from(expenses);
+  async getAllExpenses(userId, queryParams){
+    let result = db.select().from(expenses);
+    if (queryParams.category) {
+      const categoryName = queryParams.category.toLowerCase().trim();
+      const category = await this.getCategoryByName(categoryName);
+      result = result.where(eq(expenses.categoryId, category.id));
+    }
+    if(queryParams.paymentMethod){
+      const method = queryParams.paymentMethod.toLowerCase().trim();
+      result = result.where(eq(expenses.paymentMethod, method));
+    }
+    if(queryParams.startDate) result = result.where(gte(expenses.transactionDate, queryParams.startDate));
+    if (queryParams.endDate) result = result.where(lte(expenses.transactionDate, queryParams.endDate));
+
+    if (queryParams.minAmount)result =result.where(gte(expenses.amount, queryParams.minAmount));
+    if (queryParams.maxAmount) result = result.where(lte(expenses.amount, queryParams.maxAmount));
+    
+    return await result;
   }
 }
 module.exports=new ExpenseRepository();
