5d9660ff1b8d097c451e8894a29d22bbf313cf7c
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-11 13:36:36 +0530

    feat: add get_all_expense route

--- src/routes/expense.route.js
+++ src/routes/expense.route.js
@@ -6,5 +6,6 @@ const expenseController = require('../controllers/expense.controller.js');
 const { createExpenseMiddleware } = require('../middlewares/expense.middleware.js');
 
 router.post('/', createExpenseMiddleware, expenseController.createExpense);
+router.get('/',expenseController.getAllExpenses)
 
 module.exports = router;
