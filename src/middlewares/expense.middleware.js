4bef517be300b41e45ea5901d1e6c49c00d19a92
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-11 14:37:55 +0530

    feat: create update expense middleware

--- src/middlewares/expense.middleware.js
+++ src/middlewares/expense.middleware.js
@@ -1,5 +1,5 @@
 // src/middlewares/expense.middleware.js
-const { createExpenseSchema } = require('../validations/expense.validation');
+const { createExpenseSchema, updateExpenseSchema } = require('../validations/expense.validation');
 
 const createExpenseMiddleware = (req, res, next) => {
   const result = createExpenseSchema.safeParse(req.body);
@@ -17,4 +17,33 @@ const createExpenseMiddleware = (req, res, next) => {
   next();
 };
 
-module.exports = { createExpenseMiddleware };
+const updateExpenseMiddleware = (req, res, next) => {
+  const restrictedFields = ['id', 'createdAt', 'user_id'];
+  for (const field of restrictedFields) {
+    if (req.body[field] !== undefined) {
+      return res.status(400).json({
+        error: {
+          code: 'INVALID_UPDATE_FIELDS',
+          message: 'id, createdAt, and user_id cannot be updated',
+        },
+      });
+    }
+  }
+
+  const result = updateExpenseSchema.safeParse(req.body);
+
+  if (!result.success) {
+    const error = result.error.issues[0];
+    return res.status(400).json({
+      error: {
+        code: 'INVALID_EXPENSE_DATA',
+        message: error.message,
+        field: error.path[0],
+      },
+    });
+  }
+
+  req.body = result.data;
+  next();
+};
+module.exports = { createExpenseMiddleware,updateExpenseMiddleware };
