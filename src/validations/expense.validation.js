2e14bd02b4a4c25bf247f684b020d8d4c3b3bf6f
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-11 14:36:42 +0530

    feat: add update expense valdiation schema

--- src/validations/expense.validation.js
+++ src/validations/expense.validation.js
@@ -14,26 +14,16 @@ const amountValidation = z.number().positive('Amount must be greater than 0');
 
 
 const transactionDateValidation = z.string().refine((dateStr) => {
-  const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
-  if (!dateRegex.test(dateStr)) return false;
   const [year, month, day] = dateStr.split('-').map(Number);
-  const date = new Date(year, month - 1, day);
-  if (
-    date.getFullYear() !== year ||
-    date.getMonth() !== month - 1 ||
-    date.getDate() !== day
-  ) {
-    return false;
-  }
-    const now = new Date();
-  const fyStartYear =
-    now.getMonth() >= 3 ? now.getFullYear() : now.getFullYear() - 1;
-  const fyStart = new Date(fyStartYear, 3, 1);  
-  const fyEnd = new Date(fyStartYear + 1, 2, 31); 
+  const date = new Date(year, month - 1, day)
+  if (isNaN(date.getTime())) return false;
+  const now = new Date();
+  const fyStartYear = now.getMonth() >= 3 ? now.getFullYear() : now.getFullYear() - 1;
+  const fyStart = new Date(fyStartYear, 3, 1);
+  const fyEnd = new Date(fyStartYear + 1, 2, 31);
+
   return date >= fyStart && date <= fyEnd;
-}, {
-  message: 'Transaction date must be a valid date within the current financial year'
-});
+}, { message: 'Transaction date must be within current financial year' });
 
 const createExpenseSchema = z
   .object({
@@ -44,5 +34,6 @@ const createExpenseSchema = z
     amount: amountValidation,
     transactionDate: transactionDateValidation,
   })
+const updateExpenseSchema = createExpenseSchema.partial();
 
-module.exports = {createExpenseSchema};
+module.exports = {createExpenseSchema,updateExpenseSchema};
