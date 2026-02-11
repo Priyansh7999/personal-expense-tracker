ad12bdde1e99609a15d80a42a4b9d6ca0899c3d5
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-11 09:07:23 +0530

    fix: correct the characters limit for title in validation

--- src/validations/expense.validation.js
+++ src/validations/expense.validation.js
@@ -4,8 +4,8 @@ const PAYMENT_METHODS = ['cash', 'credit card', 'debit card', 'upi'];
 const CATEGORIES = ['emi', 'food', 'transport', 'healthcare', 'rent', 'other'];
 
 
-const titleValidation = z.string().trim().min(3, 'Title must be at least 3 characters').max(100, 'Title cannot exceed 100 characters');
-const descriptionValidation = z.string().trim().min(5, 'Description must be at least 5 characters').max(1000, 'Description cannot exceed 1000 characters');
+const titleValidation = z.string().trim().min(1, 'Title must be at least 1 characters').max(100, 'Title cannot exceed 100 characters');
+const descriptionValidation = z.string().trim().min(1, 'Description must be at least 1 characters').max(1000, 'Description cannot exceed 1000 characters');
 const categoryValidation = z.string().trim().transform((val) => val.toLowerCase()).refine((val) => CATEGORIES.includes(val), {
     message: 'Invalid category',
 });
